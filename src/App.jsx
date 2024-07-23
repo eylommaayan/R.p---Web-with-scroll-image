a7f83dd8134823cb9d185c5c14437942e0a80a46
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-07-23 22:16:40 +0300

    התחלה contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Intro from "../src/components/Intro/Intro";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import ProductList from "./components/productList/ProductList";
 function App() {
   return (
     <>
       <Intro />
+      <About />
+      <ProductList />
+      <Contact />
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,33 @@
+import plumber1 from "./img/1.jpg";
+import plumber2 from "./img/2.jpg";
+import plumber3 from "./img/3.jpg";
+import plumber4 from "./img/4.jpg";
+import plumber5 from "./img/5.jpg";
+import plumber6 from "./img/6.jpg";
+
+export const products = [
+  {
+    id: 1,
+    img: plumber1,
+  },
+  {
+    id: 2,
+    img: plumber2,
+  },
+  {
+    id: 3,
+    img: plumber3,
+  },
+  {
+    id: 4,
+    img: plumber4,
+  },
+  {
+    id: 5,
+    img: plumber5,
+  },
+  {
+    id: 6,
+    img: plumber6,
+  },
+];
