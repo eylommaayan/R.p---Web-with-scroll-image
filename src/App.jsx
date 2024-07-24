c4c74f114bf81b95b5efea9e382fdd93d6c3c9fe
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-07-24 11:24:23 +0300

    עדכום contecet withpout emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import Intro from "../src/components/Intro/Intro";
 import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
 import ProductList from "./components/productList/ProductList";
+import Contact from "./components/contact/Contact";
+
 function App() {
   return (
     <>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,54 +2,32 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useContext, useRef, useState } from "react";
-import emailjs from "emailjs-com";
-import { ThemeContext } from "../../context";
+import React, { useRef } from "react";
 
 const Contact = () => {
   const formRef = useRef();
-  const [done, setDone] = useState(false);
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_rrvnzco",
-        "template_3v5nih4",
-        formRef.current,
-        "user_DrriDPTGKO2Zj4RDXCA6W"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
   };
-
   return (
     <div className="c">
       <div className="c-bg"></div>
       <div className="c-wrapper">
         <div className="c-left">
-          <h1 className="c-title">Let's discuss your project</h1>
+          <h1 className="c-title">Lets discuss your project</h1>
           <div className="c-info">
             <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              +1 1234 556 75
+              <img src={Phone} alt="Phone" className="c-icon" />
+              0555555555
             </div>
             <div className="c-info-item">
-              <img className="c-icon" src={Email} alt="" />
-              contact@lama.dev
+              <img src={Email} alt="Email" className="c-icon" />
+              email@example.com
             </div>
             <div className="c-info-item">
-              <img className="c-icon" src={Address} alt="" />
-              245 King Street, Touterie Victoria 8520 Australia
+              <img src={Address} alt="Address" className="c-icon" />
+              123 Address St, City, Country
             </div>
           </div>
         </div>
@@ -58,33 +36,12 @@ const Contact = () => {
             <b>What’s your story?</b> Get in touch. Always available for
             freelancing if the right project comes along. me.
           </p>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input
-              style={{ backgroundColor: darkMode && "#333" }}
-              type="text"
-              placeholder="Name"
-              name="user_name"
-            />
-            <input
-              style={{ backgroundColor: darkMode && "#333" }}
-              type="text"
-              placeholder="Subject"
-              name="user_subject"
-            />
-            <input
-              style={{ backgroundColor: darkMode && "#333" }}
-              type="text"
-              placeholder="Email"
-              name="user_email"
-            />
-            <textarea
-              style={{ backgroundColor: darkMode && "#333" }}
-              rows="5"
-              placeholder="Message"
-              name="message"
-            />
-            <button>Submit</button>
-            {done && "Thank you..."}
+          <form ref={fromRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Name" name="user_name" />
+            <textarea rows="5" placeholder="הודעה" name="message" />
+            <button>שלח</button>
           </form>
         </div>
       </div>
